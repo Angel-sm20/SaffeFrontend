cf5291f74b641c92f3e3e9a9f523dc303d798b9b
Toshiro-19 <tu-correo-de-github@ejemplo.com>
2026-09-20 16:48:14 -0500

    feat: pasar BACKEND_URL a las vistas EJS

--- app/controllers/controller.views.js
+++ app/controllers/controller.views.js
@@ -1,6 +1,8 @@
-export const getLogin = (req, res) => res.render("login");
-export const getRegistrarse = (req, res) => res.render("registrarse");
-export const getEscaneo = (req, res) => res.render("escaneo");
-export const getAccesoAutorizado = (req, res) => res.render("acceso_autorizado");
-export const getAccesoDenegado = (req, res) => res.render("acceso_denegado");
-export const getDetalleUsuario = (req, res) => res.render("detalle_usuario");
+const apiUrl = process.env.BACKEND_URL || "http://localhost:3000";
+
+export const getLogin = (req, res) => res.render("login", { apiUrl });
+export const getRegistrarse = (req, res) => res.render("registrarse", { apiUrl });
+export const getEscaneo = (req, res) => res.render("escaneo", { apiUrl });
+export const getAccesoAutorizado = (req, res) => res.render("acceso_autorizado", { apiUrl });
+export const getAccesoDenegado = (req, res) => res.render("acceso_denegado", { apiUrl });
+export const getDetalleUsuario = (req, res) => res.render("detalle_usuario", { apiUrl });
